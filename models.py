5671bd8144be932b88834537da7ef355a249c7d5
AamirSohail07000 <sohail.aamir07000@gmail.com>
2025-03-05 06:00:18 +0530

    updated quantity

--- models.py
+++ models.py
@@ -1,10 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
+# Initialize the database
 db = SQLAlchemy()
 
+# Product Model
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    quantity = db.Column(db.Integer, default=0)
+    quantity = db.Column(db.Float, nullable=False)  # Supports decimal values
+    unit = db.Column(db.String(20), nullable=False)  # Add this line
     price = db.Column(db.Float, nullable=False)
     category = db.Column(db.String(50), nullable=False)
+
